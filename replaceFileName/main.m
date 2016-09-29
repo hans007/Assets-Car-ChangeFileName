4b77bc49d7e99281dc5494a1d36ed426163fc68d
七把刀 <tao.hans@gmail.com>
2016-09-29 14:49:57 +0800

    Initial Commit

---
+++ replaceFileName/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  replaceFileName
+//
+//  Created by hans on 16/9/29.
+//  Copyright © 2016年 yuu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
